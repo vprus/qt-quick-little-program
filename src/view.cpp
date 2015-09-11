089cf040cef5dd392c824b67befc3ce66251100a
Vladimir Prus <vladimir.prus@gmail.com>
2015-09-11 10:41:56 +0300

    Example of style singleton initialization prior to loading UI.

--- src/view.cpp
+++ src/view.cpp
@@ -4,6 +4,8 @@
 #include <QQmlEngine>
 #include <QQmlContext>
 
+#include <QQmlComponent>
+
 View::View()
 {
     setTitle("Qt Quick Little Program");
@@ -11,6 +13,19 @@ View::View()
     setResizeMode(QQuickView::SizeViewToRootObject);
 
     engine()->addImportPath(":/qml");
+
+    QQmlComponent component(engine());
+    const char *source =
+            "import QtQuick 2.0\n"
+            "import Components 1.0\n"
+            "QtObject {\n"
+            "    property var style: CustomStyle\n"
+            "    function setColor(c) { CustomStyle.headerColor = c; }\n"
+            "}";
+    component.setData(source, QUrl());
+    QObject *item = component.create();
+    QObject *style = qvariant_cast<QObject*>(item->property("style"));
+    style->setProperty("headerColor", QColor(255, 0, 0));
 
     setSource(QStringLiteral("qrc:/qml/View.qml"));
 }
